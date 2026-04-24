R3velation/ruffle

commit 447bc23c300adf6745acd0f12f4521d4e213165d

core: Make asfunction: URL prefix case insensitive (close ruffle-rs#23514)
In AVM1, executing a function from a URL is case insensitive.  The original implementation used `strip_prefix` which is case sensitive.

This patch changes the one-liner `strip_prefix` out for a string split at the point where the `asfunction:` section would end, and then matches with `eq_ignore_case`.  This selects the required prefix without clobbering the case on the address.

core/src/display_object/edit_text.rs
@@ -2468,11 +2468,12 @@ impl<'gc> EditText<'gc> {
     }
 
     fn open_url(self, context: &mut UpdateContext<'gc>, url: &WStr, target: &WStr) {
-        if let Some(address) = url.strip_prefix(WStr::from_units(b"asfunction:")) {
+        let (prefix, address) = url.split_at(11);
+        if prefix.eq_ignore_case(WStr::from_units(b"asfunction:")) {
             if let Err(e) = self.execute_avm1_asfunction(context, address) {
                 error!("Couldn't execute URL \"{url:?}\": {e:?}");
             }
-        } else if let Some(address) = url.strip_prefix(WStr::from_units(b"event:")) {
+        }  else if let Some(address) = url.strip_prefix(WStr::from_units(b"event:")) {
             if let Some(object) = self.object2() {
                 let mut activation = Avm2Activation::from_nothing(context);
                 let text = AvmString::new(activation.gc(), address);

commit f795c69ee15c9644c75d4bb837ee24cba57dec0e

core: Split URl at colon
This patch corrects a regression introduced with 447bc23c300adf6745acd0f12f4521d4e213165d
Instead of splitting at a static length, we now split at the first colon in the URL, if any.

core/src/display_object/edit_text.rs
@@ -2468,18 +2468,27 @@ impl<'gc> EditText<'gc> {
     }
 
     fn open_url(self, context: &mut UpdateContext<'gc>, url: &WStr, target: &WStr) {
-        let (prefix, address) = url.split_at(11);
-        if prefix.eq_ignore_case(WStr::from_units(b"asfunction:")) {
-            if let Err(e) = self.execute_avm1_asfunction(context, address) {
-                error!("Couldn't execute URL \"{url:?}\": {e:?}");
-            }
-        }  else if let Some(address) = url.strip_prefix(WStr::from_units(b"event:")) {
-            if let Some(object) = self.object2() {
-                let mut activation = Avm2Activation::from_nothing(context);
-                let text = AvmString::new(activation.gc(), address);
-                let event = Avm2EventObject::text_event(&mut activation, "link", text, true, false);
+        if let Some(prefix_end) = url.find(WStr::from_units(b":")) {
+            let (prefix, address) = url.split_at(prefix_end + 1);
+            if prefix.eq_ignore_case(WStr::from_units(b"asfunction:")) {
+                if let Err(e) = self.execute_avm1_asfunction(context, address) {
+                    error!("Couldn't execute URL \"{url:?}\": {e:?}");
+                }
+            } else if let Some(address) = url.strip_prefix(WStr::from_units(b"event:")) {
+                if let Some(object) = self.object2() {
+                    let mut activation = Avm2Activation::from_nothing(context);
+                    let text = AvmString::new(activation.gc(), address);
+                    let event =
+                        Avm2EventObject::text_event(&mut activation, "link", text, true, false);
 
-                Avm2::dispatch_event(activation.context, event, object.into());
+                    Avm2::dispatch_event(activation.context, event, object.into());
+                }
+            } else {
+                context.navigator.navigate_to_url(
+                    &url.to_utf8_lossy(),
+                    &target.to_utf8_lossy(),
+                    None,
+                );
             }
         } else {
             context